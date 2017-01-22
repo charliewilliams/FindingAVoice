63c7e6618c7f1aec3f3b25c764599781e8397423
Charlie Williams <c@charliewilliams.org>
2017-01-22 20:35:15 +0000

    Add “time’s up” screen

---
+++ FindingAVoice/TimesUpLockViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TimesUpLockViewController.swift
+//  FindingAVoice
+//
+//  Created by Charlie Williams on 22/01/2017.
+//  Copyright © 2017 Charlie Williams. All rights reserved.
+//
+
+import UIKit
+
+class TimesUpLockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // TODO poll periodically to see if it's tomorrow yet
+    }
+    
+    @IBAction func secretBypassGestureDetected(_ sender: UITapGestureRecognizer) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
