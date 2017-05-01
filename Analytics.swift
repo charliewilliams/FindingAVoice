7f45ab47552ad49091dbaabd3f952d5ef1085cb3
Charlie Williams <c@charliewilliams.org>
2017-05-01 17:47:45 +0100

    Basic Analytics class

---
+++ Analytics.swift
@@ -0,0 +1,22 @@
+//
+//  Analytics.swift
+//  FindingAVoice
+//
+//  Created by Charlie Williams on 01/05/2017.
+//  Copyright © 2017 Charlie Williams. All rights reserved.
+//
+
+import Foundation
+
+struct Analytics {
+
+    static func song(_ song: Song, markedAs: Song.KnowledgeLevel) {
+        
+        
+    }
+    
+    static func userRelaunchedAfterTimeUp() {
+        
+        
+    }
+}
