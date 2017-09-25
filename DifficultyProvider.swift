c21acef6aa56578edcb944579c7a520bb9c127c7
Charlie Williams <c@charliewilliams.org>
2017-09-25 14:03:27 +0100

    Work on difficulty provider

--- DifficultyProvider.swift
+++ DifficultyProvider.swift
@@ -8,9 +8,45 @@
 
 import Foundation
 
+/// Provide a difficulty level based on where the subject
+/// is in the study (first day, second day, etc)
+
 struct DifficultyProvider {
-    
+
     static var currentDifficulty: Difficulty {
         return .medium
+    }
+
+    // Calculating which day we're on
+
+    static private var daysInStudy = 14
+    static private var secondsPerDay: TimeInterval = 60*60*24
+
+    private enum Keys: String {
+        case firstDayOfStudy
+    }
+
+    static private var currentDay: Int {
+
+        guard let firstDayOfStudy = firstDayOfStudy else {
+            studyBegan()
+            return 0
+        }
+
+        let secondsSinceStartOfStudy = Date().timeIntervalSince(firstDayOfStudy)
+
+        return Int(secondsSinceStartOfStudy / secondsPerDay)
+    }
+
+    static private var firstDayOfStudy: Date? {
+        return store.value(forKey: Keys.firstDayOfStudy.rawValue) as? Date
+    }
+
+    static private func studyBegan() {
+        store.setValue(Date(), forKey: Keys.firstDayOfStudy.rawValue)
+    }
+
+    static private var store: UserDefaults {
+        return UserDefaults.standard
     }
 }
