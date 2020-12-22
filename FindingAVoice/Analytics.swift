297564b48e4e16b0ab015e69334a1f8dabd7dc71
dianaOmigie <d.omigie@gmail.com>
2020-12-22 12:53:24 +0000

    firebase2

--- FindingAVoice/Analytics.swift
+++ FindingAVoice/Analytics.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import Firebase
+import FirebaseCore
 
 struct Analytics {
     
--- FindingAVoice/ServerCoordinator.swift
+++ FindingAVoice/ServerCoordinator.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import Firebase
+import FirebaseCore
 import SVProgressHUD
 
 struct ServerCoordinator {
